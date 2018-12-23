a594eb4fb6858e1f892d16b3cd441cae7bc8bc72
Dhiraj <Dhiraj@Dhiraj-PC>
2018-12-23 09:16:00 -0500

    Xpath issue resolve and new test cases updated in LauncherTest.java

--- CLXTest/src/main/java/com/clx/qa/pages/Launcher.java
+++ CLXTest/src/main/java/com/clx/qa/pages/Launcher.java
@@ -1,6 +1,7 @@
 package com.clx.qa.pages;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
@@ -13,38 +14,89 @@ public class Launcher extends TestBase {
 	@FindBy(xpath = "//span[contains(text(), 'LaunchPad')]")
 	WebElement LaunchPadLogo;
 
-	@FindBy(xpath = "//*[@id=\"myElement\"]/div[2]/div/div[3]/div/div/div[2]/div[1]/div/div/span")
+	@FindBy(xpath = "//div[@class='details-block']//span[@style='cursor: pointer;' and contains(text(), 'MATTER MANAGER')]")
 	WebElement MatterManager;
 
-	@FindBy(xpath = "//*[@id=\"myElement\"]/div[2]/div/div[3]/div/div/div[2]/div[2]/div/div/span")
+	@FindBy(xpath = "//div[@class='details-block']//span[@style='cursor: pointer;' and contains(text(), 'INTAKE MANAGER')]")
 	WebElement IntakeManager;
 
-	//Initialization of Page Object:
+	@FindBy(xpath = "//div[@class='details-block']//span[@style='cursor: pointer;' and contains(text(), 'REFERRAL ENGINE')]")
+	WebElement ReferralEngine;
+
+	@FindBy(xpath = "//div[@class='details-block']//span[@style='cursor: pointer;' and contains(text(), 'DIGITAL ARCHIVER')]")
+	WebElement DigitalArchival;
+	
+	@FindBy(xpath="//div[@class='details-block']//span[@class='title cursor-pointer' and contains(text(), 'CONTACT MANAGER')]")
+	WebElement ContactManager;
+	
+	@FindBy(xpath="//div[@class='details-block']//span[@style='cursor: pointer;' and contains(text(), 'MARKETPLACE')]")
+	WebElement Marketplace;
+
+	@FindBy(xpath = "//div[@class='padding-left-0 margin-top10px col-md-6 ng-binding']")
+	WebElement userNameLabel;
+
+	/*
+	 * INITILAZATING OF PAGE OBJECT FOR LAUCHER PAGE
+	 */
+
 	public Launcher() {
 		PageFactory.initElements(driver, this);
 	}
-	
-	
-	//Actions
+
+	/*
+	 * LIST OF THE ACTION FOR LAUNCHER TEST CLASS
+	 */
 	public String verifyLauncherTitlePage() {
 		return driver.getTitle();
 	}
-	
+
 	public MatterManager clickOnMatterManager() {
-		MatterManager.click();
+		Actions act = new Actions(driver);
+		act.moveToElement(MatterManager).click().build().perform();
 		return new MatterManager();
 	}
-	
+
 	public IntakeManager clickOnIntakeManager() {
-		IntakeManager.click();
+		Actions act = new Actions(driver);
+		act.moveToElement(IntakeManager).click().build().perform();
 		return new IntakeManager();
 	}
+
+	public DigitalArchival clickonDigitalArchival() {
+		Actions act = new Actions(driver);
+		act.moveToElement(DigitalArchival).click().build().perform();
+		return new DigitalArchival();
+
+	}
+
+	public ReferralEngine clickonReferralEngine() {
+		Actions act = new Actions(driver);
+		act.moveToElement(ReferralEngine).click().build().perform();
+		return new ReferralEngine();
+
+	}
 	
+	public ContactManager clickonContactManager() {
+		Actions act = new Actions(driver);
+		act.moveToElement(ContactManager).click().build().perform();
+		return new ContactManager();
+
+	}
+	
+	public Marketplace clickonMarketplace() {
+		Actions act = new Actions(driver);
+		act.moveToElement(Marketplace).click().build().perform();
+		return new Marketplace();
+
+	}
+
 	public boolean validateLauncherLogo() {
 		return LaunchPadLogo.isDisplayed();
 
 	}
-	
-	
+
+	public boolean correctUserName() {
+		return userNameLabel.isDisplayed();
+	}
 
 }
--- CLXTest/src/test/java/com/clx/qa/testcases/LauncherTest.java
+++ CLXTest/src/test/java/com/clx/qa/testcases/LauncherTest.java
@@ -6,13 +6,26 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import com.clx.qa.base.TestBase;
+import com.clx.qa.pages.ContactManager;
+import com.clx.qa.pages.DigitalArchival;
+import com.clx.qa.pages.IntakeManager;
 import com.clx.qa.pages.Launcher;
 import com.clx.qa.pages.LoginPage;
+import com.clx.qa.pages.Marketplace;
+import com.clx.qa.pages.MatterManager;
+import com.clx.qa.pages.ReferralEngine;
 
 public class LauncherTest extends TestBase {
 
 	LoginPage loginPage;
 	Launcher launcher;
+	MatterManager MM;
+	IntakeManager IM;
+	DigitalArchival DA;
+	ReferralEngine RE;
+	ContactManager CM;
+	Marketplace MP;
+	
 
 	public LauncherTest() {
 		super();
@@ -22,19 +35,64 @@ public class LauncherTest extends TestBase {
 	public void setUp() throws Exception {
 		initialization();
 		loginPage = new LoginPage();
+		MM = new MatterManager();
 		launcher = loginPage.Login(prop.getProperty("username"), prop.getProperty("password"));
 		Thread.sleep(20000);
 	}
 
-	@Test
+	@Test(priority=1)
 	public void verifyLauncherTitleTest() {
 		String launcherTitle = launcher.verifyLauncherTitlePage();
 		Assert.assertEquals(launcherTitle, "Welcome to CloudLex | CloudLex", "Title Not Match");
 	}
 	
-	@Test
+	@Test(priority=2)
+	public void verifyUserNameTest() {
+		Assert.assertTrue(launcher.correctUserName());
+	}
 	
-
+	@Test(priority=3)
+	public void verifyMatterManagerLinkTest() throws Exception {
+		launcher.clickOnMatterManager();
+		MM = launcher.clickOnMatterManager();
+		Thread.sleep(20000);
+	}
+	
+	@Test(priority=4)
+	public void verifyIntakeManagerLinkTest() throws Exception {
+		launcher.clickOnIntakeManager();
+		IM = launcher.clickOnIntakeManager();
+		Thread.sleep(20000);
+	}
+	
+	@Test(priority=5)
+	public void verifyDigitalArchivalLinkTest() throws Exception {
+		launcher.clickonDigitalArchival();
+		DA = launcher.clickonDigitalArchival();
+		Thread.sleep(20000);
+	}
+	
+	@Test(priority=6)
+	public void verifyReferralEngineLinkTest() throws Exception {
+		launcher.clickonReferralEngine();
+		RE = launcher.clickonReferralEngine();
+		Thread.sleep(20000);
+	}
+	
+	@Test(priority=7)
+	public void verifyContactManagerLinkTest() throws Exception {
+		launcher.clickonContactManager();
+		CM = launcher.clickonContactManager();
+		Thread.sleep(20000);
+	}
+	
+	@Test(priority=8)
+	public void verifyMarketplaceLinkTest() throws Exception {
+		launcher.clickonMarketplace();
+		MP = launcher.clickonMarketplace();
+		Thread.sleep(20000);
+	}
+	
 	@AfterMethod
 	public void tearDown() {
 		driver.quit();
