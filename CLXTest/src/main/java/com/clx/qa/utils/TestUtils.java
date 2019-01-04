921cc64df04a711410ea1a9a082ce1466de3ad6b
CEPL <CEPL@Dhiraj-Laptop>
2019-01-04 04:48:53 -0500

    New Test Cases Added

--- CLXTest/src/main/java/com/clx/qa/utils/TestUtils.java
+++ CLXTest/src/main/java/com/clx/qa/utils/TestUtils.java
@@ -10,6 +10,7 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -25,8 +26,7 @@ public class TestUtils extends TestBase{
 	static Sheet sheet;
 	static JavascriptExecutor js;
 
-	public static String TESTDATA_SHEET_PATH = "E:/Dhiraj Project/CLX_Project/CLXTest/src/main/"
-			+ "java/com/clx/qa/testdata/CLXTestData.xlsx";
+	public static String TESTDATA_SHEET_PATH = "D:\\Dhiraj Workplace\\CLX_Project\\CLXTest\\src\\main\\java\\com\\clx\\qa\\testdata\\CLXTestData.xlsx";
 
 	public static Object[][] getTestData(String sheetName) {
 		FileInputStream file = null;
@@ -51,10 +51,20 @@ public class TestUtils extends TestBase{
 		}
 		return data;
 	}
-	
+
 	public static void takeScreenshotAtEndOfTest() throws IOException {
 		File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
 		String currentDir = System.getProperty("user.dir");
 		FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
+	}
+
+	public void removeSpinner() throws Exception
+	{
+		System.out.println("hello");
+		boolean spinner = driver.findElement(By.xpath("//*[@class='lexvia-loader ng-isolate-scope']/img")).isDisplayed();
+		while(driver.findElement(By.xpath("//*[@class='lexvia-loader ng-isolate-scope']/img")).isDisplayed())
+		{
+			Thread.sleep(5000);
+		}
 	}
 }
--- CLXTest/src/test/java/com/clx/qa/testcases/ContactManagerTest.java
+++ CLXTest/src/test/java/com/clx/qa/testcases/ContactManagerTest.java
@@ -3,6 +3,7 @@ package com.clx.qa.testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.clx.qa.base.TestBase;
@@ -42,22 +43,26 @@ public class ContactManagerTest extends TestBase {
 		Thread.sleep(20000);
 	}
 
-	/*
-	 * @DataProvider public Object[][] getCRMTestData(){ Object data[][] =
-	 * TestUtils.getTestData(sheetName); return data; }
-	 */
+	@DataProvider
+	public Object[][] getContactTestData() {
+		Object data[][] = TestUtils.getTestData(sheetName);
+		return data;
+	}
 
-	@Test(priority = 2)
-	public void validateAddNewContactTest() throws Exception {
-		CM.validateAddNewContact("Automation", "Company", "Robert", "Ned Eddard", "Stark", "test@yopmail.com",
-				"9876543456");
+	@Test(priority = 2, dataProvider="getContactTestData")
+	public void validateAddNewContactTest(String spec, String comp, String fname, String mname, String lname, String email,
+			String cell) throws Exception {
+		
+		//CM.validateAddNewContact("Automation", "Company", "Robert", "Ned Eddard", "Stark", "test@yopmail.com","9876543456");
+		
+		
+		CM.validateAddNewContact(spec, comp, fname, mname, lname, email, cell);
 		Thread.sleep(5000);
 		try {
 			CM.handleDuplicateContact();
-			System.out.println("Conatct is Duplicate");
+			System.out.println("Contact is Duplicate");
 			Thread.sleep(10000);
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Contact is not Duplicate");
 		}
 	}
